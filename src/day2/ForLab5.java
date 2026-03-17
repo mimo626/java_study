e72299bba80f618812a17142cb18fb056bc7b9cc
kangminju <yessuk5499@gmail.com>
2026-03-17 17:24:08 +0900

    ForLab5 실습 구현

---
+++ src/day2/ForLab5.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class ForLab5 {
+
+	public static void main(String[] args) {
+		int evenNum = 0;
+		int oddNum = 0;
+		
+		for(int i = 1; i<=100; i++) {
+			if(i%2==0) 
+				evenNum += i;
+			else 
+				oddNum += i;
+		}
+		
+		System.out.print("1부터 100까지의 숫자들 중에\n짝수의 합은 " + evenNum + " 이고\n" +
+				"홀수의 합은 " + oddNum + " 이다.");
+	}
+
+}
