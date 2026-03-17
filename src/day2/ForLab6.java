68a7f8b8242dfe7c88cdfc2c0b43d8cd73617755
kangminju <yessuk5499@gmail.com>
2026-03-17 17:38:57 +0900

    틀린 부분 수정

--- src/day2/ForLab6.java
+++ src/day2/ForLab6.java
@@ -3,18 +3,18 @@ package day2;
 public class ForLab6 {
 
 	public static void main(String[] args) {
-		int thrToTen = (int)(Math.random()*7 + 3);
+		int thrToTen = (int)(Math.random()*8 + 3); // 3~10범위이므로 *7이 아닌 8
 		int oneToThr = (int)(Math.random()*3 + 1);
-		String specialCode;
+		final String SPECIALCODE; // 한번 선언 후 초기화는 한번만 하므로 final 상수 가능함.
 
 		switch(oneToThr) {
-			case 1: specialCode = "*"; break;
-			case 2: specialCode = "$"; break;
-			default: specialCode = "#";
+			case 1: SPECIALCODE = "*"; break;
+			case 2: SPECIALCODE = "$"; break;
+			default: SPECIALCODE = "#";
 		}
 
 		for(int i = 1; i <= thrToTen; i++) {
-			System.out.print(specialCode);
+			System.out.print(SPECIALCODE);
 		}
 
 	}
