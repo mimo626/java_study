912482496980cd12515d2e235c0ef64f417339b1
kangminju <yessuk5499@gmail.com>
2026-03-17 12:41:04 +0900

    OperOrLab 실습 구현

---
+++ src/day2/OperOrLab.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class OperOrLab {
+
+	public static void main(String[] args) {
+		int grade = (int)(Math.random() * 6) + 1;
+		if(grade == 1 || grade == 2 || grade == 3) {
+			System.out.printf("%d학년은 저학년입니다.", grade);
+		} else {
+			System.out.printf("%d학년은 고학년입니다.", grade);
+		}
+	}
+}
