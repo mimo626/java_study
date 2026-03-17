d196d7765cb8d0c41e42d280ff0231e27487face
kangminju <yessuk5499@gmail.com>
2026-03-17 12:41:30 +0900

    SwitchLab2 실습 구현

---
+++ src/day2/SwitchLab2.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class SwitchLab2 {
+
+	public static void main(String[] args) {
+		int randomNum = (int)(Math.random()*5) + 1;
+		int result = 0;
+		switch(randomNum) {
+			case 1: result = 300 + 50; break;
+			case 2: result = 300 - 50; break;
+			case 3: result = 300 * 50; break;
+			case 4: result = 300 / 50; break;
+			case 5: result = 300 % 50; break;
+		}
+		
+		System.out.printf("결과값 : %d", result);
+
+	}
+
+}
