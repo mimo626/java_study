2d05bcf69517b3893fc96e4b33ace9b11113f6ba
kangminju <yessuk5499@gmail.com>
2026-03-17 17:23:21 +0900

    ForLab2 실습 구현

---
+++ src/day2/ForLab2.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class ForLab2 {
+
+	public static void main(String[] args) {
+		final String DUKE = "DUKE";
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(DUKE + i + " ");
+		}
+
+	}
+
+}
