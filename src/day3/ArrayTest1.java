e624fe2769daa15e3ad435e2c97f3744a4e84a10
kangminju <yessuk5499@gmail.com>
2026-03-18 16:05:57 +0900

    ArrayTest1 생성

---
+++ src/day3/ArrayTest1.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class ArrayTest1 {
+
+	public static void main(String[] args) {
+		int[] nums1 = new int[10];
+		int[] nums2 = new int[] {10, 20 ,30};
+		int[] nums3 = {1, 2, 3, 4, 5};
+		
+		System.out.println(nums1);
+		System.out.println(nums2);
+		System.out.println(nums3);
+		
+		System.out.println(nums1.length);
+		System.out.println(nums2.length);
+		System.out.println(nums3.length);
+		
+		System.out.println(nums1[0]);
+		System.out.println(nums2[0]);
+		System.out.println(nums3[0]);
+	}
+
+}
