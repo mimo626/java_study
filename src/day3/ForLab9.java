0c8c110cf84bba7b75d83029627fe73e45e1d0f6
kangminju <yessuk5499@gmail.com>
2026-03-18 10:17:16 +0900

    ForLab9 실습 구현

---
+++ src/day3/ForLab9.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class ForLab9 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j<i; j++) {
+				System.out.print(' ');
+			}
+			for(int j = 1; j<=10; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+	}
+
+}
