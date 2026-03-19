8644c63a2c452c6bd0ccc68202e3a75b34ff22af
kangminju <yessuk5499@gmail.com>
2026-03-19 14:48:50 +0900

    TwoArrayTest4 실습 설명 추가

--- src/day4/TwoArrayTest4.java
+++ src/day4/TwoArrayTest4.java
@@ -8,7 +8,7 @@ public class TwoArrayTest4 {
 		               		};
 		int sum = 0;
 		for(int i=0; i < nums.length; i++) 
-			for(int j=0; j < nums[i].length; j++)
+			for(int j=0; j < nums[j].length; j++)
 				sum += nums[i][j];
 		System.out.println("전체 데이터의 합 : "+sum);
 		
