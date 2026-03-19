f1fe920f39f5613bef6965e0780aa36b861a7b2f
kangminju <yessuk5499@gmail.com>
2026-03-19 17:38:42 +0900

    메서드 실습2 구현

---
+++ src/day4/MethodLab2.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class MethodLab2 {
+
+	public static void main(String[] args) {
+		System.out.println(isEven(10) ? "10은 짝수 입니다." : "10은 홀수 입니다.");
+		System.out.println(isEven(3) ? "3은 짝수 입니다." : "3은 홀 수입니다.");
+	}
+	
+	static boolean isEven(int num) {
+		if(num % 2 == 0) return true;
+		else return false;
+	}
+
+}
