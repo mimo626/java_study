3a26813ac7be1d2a5817f639edf17088f7aff78e
kangminju <yessuk5499@gmail.com>
2026-03-20 12:02:49 +0900

    MethodLab8 실습 구현

---
+++ src/day5/MethodLab8.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class MethodLab8 {
+
+	public static void main(String[] args) {
+		System.out.println("[ 실행 결과 ]\n");
+		System.out.println(isRightTriangle(3, 4, 5));
+		System.out.println(isRightTriangle(1, 2, 3));
+		
+	}
+	
+	static boolean isRightTriangle(int width, int height, int hypo) {
+		return (width*width) + (height*height) == (hypo*hypo);
+	}
+
+}
