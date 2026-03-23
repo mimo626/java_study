ae8aec57cd6cec120f76918a15b08353ead21ef8
kangminju <yessuk5499@gmail.com>
2026-03-23 17:57:27 +0900

    입력실습 수정

--- src/day6/ScannerTest4.java
+++ src/day6/ScannerTest4.java
@@ -8,7 +8,7 @@ public class ScannerTest4 {
 		System.out.printf("데이터를 입력하세요 : ");
 		String str1 = sc.next();
 		String str2 = sc.next();
-		sc.nextLine();  // 입력버퍼에 남아있는 개행문자를 청소하는 기능
+		//sc.nextLine();  // 입력버퍼에 남아있는 개행문자를 청소하는 기능
 		String line1 = sc.nextLine();
 		String line2 = sc.nextLine();
 		System.out.printf("[%s] [%s] [%s] [%s]", str1, str2, line1, line2);
