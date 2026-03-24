4cdb434867d347d09410aa403333d46e4a35d138
kangminju <yessuk5499@gmail.com>
2026-03-24 15:31:28 +0900

    Friend 실습 구현

---
+++ src/day7/Friend.java
@@ -0,0 +1,31 @@
+package day7;
+
+class Person {
+	private String name;
+	
+	Person(String name) {
+		this.name = name;
+	}
+	
+	public String getInfo() {
+		return name;
+	}
+}
+
+class Friend extends Person {
+	private String phoneNum;
+	private String email;
+	
+	Friend(String name, String phoneNum, String email){
+		super(name);
+		this.phoneNum = phoneNum;
+		this.email = email;
+	}
+	
+	public String getInfo() {
+		// 부모 클래스의 private 필드는 직접 접근할 수 없으므로 getter 사용
+		// String.format으로 형식 지정
+		return super.getInfo() + "	   " + phoneNum + "	   " + email;
+	}
+	
+}
