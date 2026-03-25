1bbd7cd65c8465aa84e2ec622431a9ca0a7289c6
kangminju <yessuk5499@gmail.com>
2026-03-25 12:02:53 +0900

    Otab 실습 구현

---
+++ src/mobile/Otab.java
@@ -0,0 +1,17 @@
+package mobile;
+
+public class Otab extends Mobile{
+	public Otab(){}
+	public Otab(String mobileName, int batterySize, String osType) {
+		super(mobileName, batterySize, osType);
+	}
+	
+	@Override
+   public void operate(int time) {
+	   setBatterySize(getBatterySize() - time*12);
+   };
+	@Override
+   public void charge(int time) {
+	   setBatterySize(getBatterySize() + time*8);
+   };
+}
