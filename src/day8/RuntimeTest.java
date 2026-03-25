ea76f310cc505003066b0ed211dec4f96c775536
kangminju <yessuk5499@gmail.com>
2026-03-25 15:00:54 +0900

    RuntimeTest 구현

--- src/day8/RuntimeTest.java
+++ src/day8/RuntimeTest.java
@@ -2,6 +2,14 @@ package day8;
 
 public class RuntimeTest {
 	public static void main(String[] args) throws Exception{
-
+		Runtime obj = Runtime.getRuntime();
+		Runtime obj2 = Runtime.getRuntime();
+		Runtime obj3 = Runtime.getRuntime();
+		
+		System.out.println(obj);
+		System.out.println(obj2);
+		System.out.println(obj3);
+		obj.exec("/System/Applications/Notes.app");
+		obj.exec("open -a Notes");
 	}
 }
