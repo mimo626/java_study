5d92ab7b62bb1cf376b25b7a6c6bfabc15ce0481
kangminju <yessuk5499@gmail.com>
2026-03-25 12:03:02 +0900

    Mobile 실습 구현

---
+++ src/mobile/Mobile.java
@@ -0,0 +1,42 @@
+package mobile;
+
+abstract public class Mobile {
+	private String mobileName;
+	private int batterySize;
+	private String osType;
+	
+	public Mobile(){}
+	
+	public Mobile(String mobileName, int batterySize, String osType) {
+		this.mobileName = mobileName;
+		this.batterySize = batterySize;
+		this.osType = osType;
+	}
+	
+	abstract public void operate(int time);
+	abstract public void charge(int time);
+
+	public String getMobileName() {
+		return mobileName;
+	}
+
+	public void setMobileName(String mobileName) {
+		this.mobileName = mobileName;
+	}
+
+	public int getBatterySize() {
+		return batterySize;
+	}
+
+	public void setBatterySize(int batterySize) {
+		this.batterySize = batterySize;
+	}
+
+	public String getOsType() {
+		return osType;
+	}
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+}
