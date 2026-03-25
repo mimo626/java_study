e05d36cade97ece8325222ca729ed12eb4ddd1c2
kangminju <yessuk5499@gmail.com>
2026-03-25 17:57:33 +0900

    RentalTV 실습 구현

---
+++ src/day8/RentalTV.java
@@ -0,0 +1,23 @@
+package day8;
+
+public class RentalTV extends TV implements Rentable {
+	private int price;
+
+	public RentalTV(){}
+	public RentalTV(int price, String model, int size, int channel) {
+		super(model, size, channel);
+		this.price = price;
+	}
+	public void play() {
+		System.out.println("대여 TV 채널 " + getChannel() + "번의 프로를 플레이 합니다.");
+	}
+	public void rent() {
+		System.out.printf("%s모델의 상품을 대여합니다. %,d원을 지불해 주세요.", getModel(), price);
+	}
+	@Override
+	public String toString() {
+		String info = String.format("대여상품정보 : 모델명(%s), 가격(%,d원), 크기(%d)",  getModel(), price, getSize());
+		return info;
+	}
+
+}
