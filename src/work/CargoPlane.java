98d948d81f7bbc5aa3cc8559993329ff2a21bab7
kangminju <yessuk5499@gmail.com>
2026-03-25 13:55:26 +0900

    CargoPlane 실습구현

---
+++ src/work/CargoPlane.java
@@ -0,0 +1,12 @@
+package work;
+
+public class CargoPlane extends Plane{
+	CargoPlane(){}
+	CargoPlane(String planeName, int fulSize){
+		super(planeName, fulSize);
+	}
+	@Override
+	public void flight(int distance) {
+		setFulSize(getFulSize() - (distance/10)*50);
+	}
+}
