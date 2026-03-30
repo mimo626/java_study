59c53e526ca95b5f847a060aff27c352b000bc97
kangminju <yessuk5499@gmail.com>
2026-03-30 11:36:52 +0900

    CopyLab 실습 수정

--- src/day10/fileio/CopyLab.java
+++ src/day10/fileio/CopyLab.java
@@ -5,30 +5,33 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class CopyLab {
 
 	public static void main(String[] args) {
 		try(FileReader reader = new FileReader("/Users/kangminju/eclipse-workspace/javaedu/src/day10/sample.txt");
+				// 여기서 true를 줘야 이전 내용 그대로 두고 뒤에 추가하는 append 모드로 전환 
 				FileWriter writer = new FileWriter("/Users/kangminju/eclipse-workspace/javaedu/src/day10/sample_new.txt", true);
-				PrintWriter out = new PrintWriter(writer);
+				// PrintWriter out = new PrintWriter(writer);
 				BufferedReader br = new BufferedReader(reader) 
 					) {
 				while (true) {
 					String data = br.readLine();
 					if (data == null)
 						break;
-					out.printf(data);
+					// out.printf(data);
+					writer.write(data + "\n");
 				}
-				System.out.println("저장이 완료되었습니다.");
-
-			} catch (FileNotFoundException fnfe) {
+			} catch (FileNotFoundException e) {
 				System.out.println("sample.txt 파일을 찾을 수 없습니다.");
+				e.printStackTrace();
+				return;
+				
 			} catch (IOException ioe) {
 				System.out.println("입출력을 처리하는 동안 오류가 발생했습니다.");
+				return;
 			}
-
+			System.out.println("저장이 완료되었습니다.");
 	}
 
 }
--- src/day10/fileio/FileOutLab.java
+++ src/day10/fileio/FileOutLab.java
@@ -17,21 +17,26 @@ public class FileOutLab {
 		}
 		try (FileWriter writer = new FileWriter("/Users/kangminju/Desktop/event.txt", true);
 				PrintWriter out = new PrintWriter(writer);) {
-			LocalDate ld1 = LocalDate.of(2026, 8, 15);
-			LocalDate ld2 = LocalDate.of(2026, 12, 25);
+			LocalDate date1 = LocalDate.of(2026, 8, 15);
+			LocalDate date2 = LocalDate.of(2026, 12, 25);
 
-			DayOfWeek day1 = ld1.getDayOfWeek();
-			DayOfWeek day2 = ld2.getDayOfWeek();
+			DayOfWeek day1 = date1.getDayOfWeek();
+			DayOfWeek day2 = date2.getDayOfWeek();
 
 			String korday1 = day1.getDisplayName(TextStyle.FULL, Locale.KOREAN);
 			String korday2 = day2.getDisplayName(TextStyle.FULL, Locale.KOREAN);
 
-			out.printf("%d년 %d월 %d일은 %s입니다.\r\n", 2026, 8, 15, korday1);
-			out.printf("%d년 %d월 %d일은 %s입니다.\r\n", 2026, 12, 25, korday2);
+			writer.write(String.format("%d년 %d월 %d일은 %s입니다.\r\n", date1.getYear(), date1.getMonthValue(), date1.getDayOfMonth(), korday1));
+			writer.write(String.format("%d년 %d월 %d일은 %s입니다.\r\n", date2.getYear(), date1.getMonthValue(), date2.getDayOfMonth(), korday2));
+			
+			writer.write(String.format("%tY년 %1$tm월 %1$td일은 %1$tA입니다.\n", date1));
+			writer.write(String.format("%tY년 %1$tm월 %1$td일은 %1$tA입니다.\n", date2));
 
 			System.out.println("저장이 완료되었습니다.");
 
 		} catch (Exception e) {
+			// 에러메시지 출력 습관적으로 하기 
+			e.printStackTrace();
 			System.out.println("파일에 저장하는 동안 오류가 발생했습니다.");
 		}
 	}
